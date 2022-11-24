49c480dbb27188a2bd20c29edbe3c0a52e2e2a2d
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-11-24 16:56:26 +0530

    client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const DataModel = require("./models/data");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 mongoose
   .connect(process.env.mango)
   .then(() => console.log("DB connection established"))
   .catch((err) => console.log(err.message));
+
+app.get("/data", async (req, res) => {
+  const allData = await DataModel.find();
+  res.send(allData);
+});
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port 8080");
